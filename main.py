a18cf55d1d40d1fd3e73574e22d3b15c7c6fd28f
satelis.8951 <maria.satelis@madeiramadeira.com.br>
2023-07-12 16:28:14 -0300

    python

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from PyQt5 import uic, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
+import webbrowser
 
 app = QtWidgets.QApplication([])
-tela = uic.loadUi("suplementos.ui")
-tela2 = uic.loadUi("untitled.ui")
+tela = uic.loadUi("pagina_de_cadastro.ui")
+tela2 = uic.loadUi("pagina_treino_em_casa.ui")
+tela3 = uic.loadUi("suplementos1.ui")
+tela4 = uic.loadUi("testee.ui")
+tela4.show()
 
-def botao():
-    tela.close()
-    tela2.show()
-
-tela.show()
-
+def testee():
+    url = "https://www.gsuplementos.com.br/"
+    webbrowser.open(url)
+    testee()
 
 app.exec()
