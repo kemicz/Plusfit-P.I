0e66203504621afb0094fbb19301551faa6ff7c3
satelis.8951 <maria.satelis@madeiramadeira.com.br>
2023-07-13 16:32:41 -0300

    duda

--- main.py
+++ main.py
@@ -2,16 +2,32 @@ from PyQt5 import uic, QtWidgets
 from PyQt5.QtWidgets import QMessageBox
 import webbrowser
 
+def castrar():
+    tela1.close()
+    tela.show()
+
+def voltaplogin():
+    tela.close()
+    tela1.show()
+
+def cadastrar():
+    tela.close()
+    tela4.show()
+
+
+
+def larginina():
+    webbrowser.open("https://www.gsuplementos.com.br/conteudo/arginina-beneficios/")
+
 app = QtWidgets.QApplication([])
 tela = uic.loadUi("pagina_de_cadastro.ui")
+tela1 = uic.loadUi("login.ui")
 tela2 = uic.loadUi("pagina_treino_em_casa.ui")
 tela3 = uic.loadUi("suplementos1.ui")
-tela4 = uic.loadUi("testee.ui")
-tela4.show()
-
-def testee():
-    url = "https://www.gsuplementos.com.br/"
-    webbrowser.open(url)
-    testee()
-
+tela4 = uic.loadUi("perfil.ui")
+tela1.cadastrarlog.clicked.connect(castrar)
+tela.voltaplogin.clicked.connect(voltaplogin)
+tela3.l_arginina.clicked.connect(larginina)
+tela.cadastrar.clicked.connect(cadastrar)
+tela1.show()
 app.exec()
